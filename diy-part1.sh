d4f34d390e7520ed8529713a6a76125f1d013326
ssnow22-star <74156772+ssnow22-star@users.noreply.github.com>
2022-03-22 15:19:28 -0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,14 +14,21 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git packages https://github.com/immortalwrt/packages.git' feeds.conf.default
+sed -i '$a src-git luci https://github.com/immortalwrt/luci.git' feeds.conf.default
+sed -i '$a src-git routing https://github.com/immortalwrt/routing.git' feeds.conf.default
+sed -i '$a src-git telephony https://github.com/openwrt/telephony.git' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git bypass https://github.com/kiddin9/openwrt-bypass.git' feeds.conf.default
+sed -i '$a src-git helmi helmi https://github.com/helmiau/helmiwrt-packages.git' feeds.conf.default
+sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom' feeds.conf.default
 
 # Define My Package
-#git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
-#git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
-#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/molun/luci-theme-infinityfreedom
+git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/molun/luci-app-adguardhome
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
+git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/molun/luci-theme-infinityfreedom
